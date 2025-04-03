a5dfd580ac047badb00703ef5c85d60780e1bab0
Shawkat <shawkattelki0007@gmail.com>
2025-04-03 22:57:43 +0200

    uno logic round end complite

--- src/app/components/games/uno/player/player.component.ts
+++ src/app/components/games/uno/player/player.component.ts
@@ -8,10 +8,9 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
-import {NgForOf, NgStyle} from '@angular/common';
+import {NgForOf} from '@angular/common';
 import { Deck } from '../services/uno/Deck';
-import {TurnRound} from '../services/uno/TurnRound';
-import {CardsContainerComponent} from '@components/games/uno/cards-container/cards-container.component';
+import {getMatIconFailedToSanitizeLiteralError} from '@angular/material/icon';
 
 @Component({
   selector: 'app-player',
@@ -32,18 +31,17 @@ export class PlayerComponent implements OnInit, OnChanges {
   private isFirstCardUsed = false;
   private player1: string = "player1";
   private player2: string = "player2";
-  private extractDrawCardColor:string = "";
+  private extractDrawCardColor: string = "";
 
   constructor(private cdr: ChangeDetectorRef, private deck: Deck) {}
 
   ngOnInit() {
-    this.validateFirstCardColor();
+    this.initializeFirstCardColor();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.validateColorCard(changes);
-    this.turnRoundOnClick(changes);
-
+    this.updateClickedCardColor(changes);
+    this.handleTurnChange(changes);
   }
 
   getCardTransform(index: number, totalCards: number): string {
@@ -55,13 +53,14 @@ export class PlayerComponent implements OnInit, OnChanges {
     return index;
   }
 
-  validateFirstCardColor(){
+  initializeFirstCardColor() {
     if (this.getFirstCard && !this.isFirstCardUsed) {
       this.colorOfCardOutPut = this.extractCardColor(this.getFirstCard);
+      this.isFirstCardUsed = true;
     }
   }
 
-  validateColorCard(changes: SimpleChanges): void {
+  updateClickedCardColor(changes: SimpleChanges): void {
     if (changes["clickedCard"]?.currentValue && changes["clickedCard"].currentValue !== changes["clickedCard"].previousValue) {
       const newColor = this.extractCardColor(this.clickedCard);
       if (newColor) {
@@ -70,124 +69,98 @@ export class PlayerComponent implements OnInit, OnChanges {
     }
   }
 
-  turnRoundOnClick(changes: SimpleChanges): void {
-    if (changes["turnRound"]?.currentValue) {
-      if (changes["turnRound"].currentValue !== changes["turnRound"].previousValue) {
-        this.nextTurn();
-        this.drawCard(this.isPlayer2 ? this.player2 : this.player1);
-      }
+  handleTurnChange(changes: SimpleChanges): void {
+    if (changes["turnRound"]?.currentValue !== changes["turnRound"]?.previousValue) {
+      this.drawCardForPlayer(this.isPlayer2 ? this.player2 : this.player1);
+      this.switchToNextPlayer();
     }
   }
 
-  discard1(card: string) {
-    if (this.isPlayer2 && this.checkPlayerCardColor(this.player1)) {
-      this.removeCard(this.player1, card);
+  playerCanPlayColor(player: string): boolean {
+    return this.players[player]?.some(card => this.extractCardColor(card) === this.colorOfCardOutPut);
+  }
+
+  playCardIfValid(card: string, player: string) {
+    console.log(this.extractNumFromCard(card));
+
+    const isCurrentPlayer = (player === this.player1 && this.isPlayer2) || (player === this.player2 && !this.isPlayer2);
+    const cardColor = this.extractCardColor(card);
+
+    if (!isCurrentPlayer) {
+      console.log("Nicht deinen Zug!")
+      return;
+    }
+
+    if (!this.playerCanPlayColor(player)) {
+      this.drawCardForPlayer(player);
+      return;
+    }
+
+    if (cardColor === this.colorOfCardOutPut) {
+      this.removeCardFromPlayer(player, card);
       this.clickedCard = card;
-      this.colorOfCardOutPut = this.extractCardColor(card);
-      this.nextTurn();
-    } else if (this.isPlayer2) {
-      this.drawCard(this.player1);
+      this.colorOfCardOutPut = cardColor;
+      this.switchToNextPlayer();
     }
   }
 
-  discard2(card: string) {
-    if (!this.isPlayer2 && this.checkPlayerCardColor(this.player2)) {
-      this.removeCard(this.player2, card);
-      this.clickedCard = card;
-      this.colorOfCardOutPut = this.extractCardColor(card);
-      if (!this.checkPlayerHasCardColor(this.player2, card)){
-        this.nextTurn();
-      }
-      console.log(this.colorOfCardOutPut);
-    } else if (!this.isPlayer2) {
-      this.drawCard(this.player2);
+  drawCardForPlayer(player: string) {
+    if (this.deck.getDeck().length === 0) {
+      console.warn("Das Deck ist leer! Karte kann nicht gezogen werden.");
+      return;
+    }
+
+    const card = this.deck.getDeck().shift();
+    if (card) {
+      this.players[player].push(card);
+      this.extractDrawCardColor = this.extractCardColor(card);
+      this.cdr.detectChanges();
+    }
+
+    if (this.extractDrawCardColor !== this.colorOfCardOutPut) {
+      this.switchToNextPlayer();
     }
   }
 
-  checkPlayerHasCardColor(player: string, card: string): boolean {
-    if (this.colorOfCardOutPut) {
-      const playerCards = this.players[player];
-      const extractCardColor = this.extractCardColor(card);
-      for ( let i = 0; i < playerCards.length; i++ ) {
-        const extractPlayerCardColor = this.extractCardColor(playerCards[i]);
-        if (extractPlayerCardColor === extractCardColor) {
-          console.log("extractPlayerCardColor", extractPlayerCardColor);
-          console.log("extractCardColor", extractCardColor);
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  checkPlayerCardColor(player: string): boolean {
-    if (this.colorOfCardOutPut) {
-      const playerCards = this.players[player];
-      for ( let i = 0; i < playerCards.length; i++ ) {
-        if (playerCards[i].includes(this.colorOfCardOutPut)){
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  nextTurn() {
-    this.isPlayer2 = !this.isPlayer2;
-  }
-
-
-  drawCard(player: string) {
-    if (this.deck.getDeck().length > 0) {
-      var card = this.deck.getDeck().shift();
-      if (card) {
-        this.players[player].push(card);
-        this.extractDrawCardColor = this.extractCardColor(card);
-        this.cdr.detectChanges();
-      }
-
-    }
-    this.nextTurn();
-  }
-
-  private removeCard(player: string, card: string) {
-    if (this.checkPlayerCardColor(player)) {
-      const index = this.players[player].indexOf(card);
-      if (index > -1) {
-        this.players[player].splice(index, 1);
-        this.players = { ...this.players };
-        this.cdr.detectChanges();
-        this.cardOutPut.emit(card);
-      }
-    } else {
-      this.drawCard(player);
+  private removeCardFromPlayer(player: string, card: string) {
+    const index = this.players[player].indexOf(card);
+    if (index > -1) {
+      this.players[player].splice(index, 1);
+      this.players = { ...this.players };
+      this.cdr.detectChanges();
+      this.cardOutPut.emit(card);
     }
   }
+
   private extractCardColor(card: string): string {
     if (!card) return "";
-
-    for (const color of this.colors) {
-      if (card.includes(color)) {
-        return color;
-      }
-    }
-    return "";
+    return this.colors.find(color => card.includes(color)) || "";
   }
 
-  endRound() {
-    const currentPlayer = this.isPlayer2 ? this.player2 : this.player1;
+  switchToNextPlayer() {
+    this.isPlayer2 = !this.isPlayer2;
 
+  }
+
+  private extractNumFromCard(card: string): number | null {
+    if (!card) return null;
+
+    const match = card.match(/(\d+)\.svg$/);
+    return match ? parseInt(match[1], 10) : null;
+  }
+
+  completeRound() {
+    const currentPlayer = this.isPlayer2 ? this.player2 : this.player1;
     const drawnCard = this.deck.getDeck().shift();
+
     if (drawnCard) {
       this.players[currentPlayer].push(drawnCard);
-      this.cdr.detectChanges();
+      console.log(`${currentPlayer} zieht eine Karte: ${drawnCard}`);
 
       if (this.extractCardColor(drawnCard) === this.colorOfCardOutPut) {
-        this.removeCard(currentPlayer, drawnCard);
+        this.removeCardFromPlayer(currentPlayer, drawnCard);
       }
+      this.switchToNextPlayer();
     }
-    this.nextTurn();
   }
-
-
 }
