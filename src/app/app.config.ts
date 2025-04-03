9e09c98005faebf0c07d8ea351db8e0157ef49c5
de63679 <shawkat.telki@gc-gmail.de>
2025-04-03 17:34:34 +0200

    optimize uno card and structur in the middle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import Aura from '@primeng/themes/aura';
 import {providePrimeNG} from 'primeng/config';
-import { provideKeycloakAngular } from '@config/keycloak.config';
-
+// import { provideKeycloakAngular } from '@config/keycloak.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     providePrimeNG({ theme: { preset: Aura } }),
-    provideKeycloakAngular()
+    // provideKeycloakAngular()
 
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import {MainPageComponent} from './components/main-page-component/main-page-component';
-import { canActivateAuthRole } from '@guards/auth.guard';
 
 export const routes: Routes = [
   // { path: '', component: MainPageComponent, canActivate: [canActivateAuthRole] },
